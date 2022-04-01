81889790e2590d7bd98bab8c2afc4bb19e33aa6e
Nannan <nanna7077@gmail.com>
2022-04-02 00:00:36 +0530

    Add: Too many things

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from email.policy import default
 from flask import (
     Flask,
     render_template,
@@ -16,45 +15,51 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Register(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False, unique=True)
-    phone = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    __tablename__="registers"
+
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String, nullable=False)
+    email=db.Column(db.String, nullable=False, unique=True)
+    phone=db.Column(db.Integer, nullable=False)
+    date=db.Column(db.DateTime, default=datetime.utcnow)
 
 @app.route('/', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        print(request.form)
-        name = request.form['name']
-        email = request.form['email']
-        phone = request.form['phone']
-
+        name=request.form['name']
+        email=request.form['email']
+        phone=request.form['phone']
         new_student = Register(name = name, email = email, phone = phone)
-
         try:
             db.session.add(new_student)
             db.session.commit()
             return redirect('/registered')
-        except:
-            return redirect('/error')
-
-
-    else:
-        datas=Register.query.order_by(Register.date)
-        print(datas)
-    return render_template('index.html')
+        except Exception as err:
+            return redirect('/error/{}'.format(str(err)))
+    return render_template('home.html')
 
 @app.route('/dashboard')
 def display():
     datas=Register.query.order_by(Register.date)
-    print(datas)
-    return render_template('dashboard.html', datas = datas)
+    return render_template('dashboard.html', datas=datas)
+
+@app.route("/dashboard/entry/delete/<id>")
+def dashboard_delete_entry(id: int):
+    entry=Register.query.get(id)
+    if entry==None:
+        return redirect("/error/Invalid%20Entry")
+    else:
+        db.session.delete(entry)
+        db.session.commit()
+        return redirect("/dashboard?message=Delete%20Successfull&type=success")
 
 @app.route('/registered')
 def registered():
     return render_template('registered.html')
 
-@app.route('/error')
-def error():
-    return render_template('error.html')
+@app.route('/error/<message>')
+def error(message: str):
+    return render_template('error.html', message=message)
+
+if __name__=="__main__":
+    app.run(debug=True)
